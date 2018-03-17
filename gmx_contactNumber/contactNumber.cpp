cb8544222d682507636af41caa79bc674e4c96d7
Yiming Tang <tym@tymworld.com>
2018-03-17 17:24:38 +0800

    Finished with codes 1.0

---
+++ gmx_contactNumber/contactNumber.cpp
@@ -0,0 +1,9 @@
+//
+//  contactNumber.cpp
+//  gmx_contactNumber
+//
+//  Created by Yiming Tang on 17/03/2018.
+//  Copyright © 2018 Yiming Tang. All rights reserved.
+//
+
+#include "contactNumber.hpp"
---
+++ gmx_contactNumber/contactNumber.hpp
@@ -0,0 +1,14 @@
+//
+//  contactNumber.hpp
+//  gmx_contactNumber
+//
+//  Created by Yiming Tang on 17/03/2018.
+//  Copyright © 2018 Yiming Tang. All rights reserved.
+//
+
+#ifndef contactNumber_hpp
+#define contactNumber_hpp
+
+#include <stdio.h>
+
+#endif /* contactNumber_hpp */
